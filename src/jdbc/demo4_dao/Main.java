92c12ad68965eeca7f0efcae96eb978f482cfbc8
Wasin Sanguansuk <wasin_sanguansuk@hotmail.com>
2021-08-19 22:34:33 -0500

    Created more userDao functions

--- src/jdbc/demo4_dao/Main.java
+++ src/jdbc/demo4_dao/Main.java
@@ -23,6 +23,22 @@ public class Main {
         }
 
         System.out.println("----------");
+        System.out.println("UPDATE set user id 1 firstName to Conea");
+
+        new UserDao().updateUserFirstName("Conea", 1);
+
+        System.out.println("----------");
+        System.out.println("INSERT a user");
+
+        new UserDao().insertUser();
+
+        System.out.println("----------");
+        System.out.println("DELETE user by id");
+        // This user has to be new (meaning that this user don't have id in other table such as userroles)
+
+        new UserDao().deleteUser(6);
+
+        System.out.println("----------");
 
         System.out.println("End of main()");
     }
--- src/jdbc/demo4_dao/UserDao.java
+++ src/jdbc/demo4_dao/UserDao.java
@@ -67,7 +67,7 @@ public class UserDao extends AbstractDao {
         return users;
     }
 
-    public List<User> updateUsers(String lastNameValue) {
+    public void updateUserFirstName(String firstNameValue, Integer idValue) {
         PreparedStatement stmt = null;
         ResultSet rs = null;
 
@@ -75,31 +75,36 @@ public class UserDao extends AbstractDao {
 
         try {
             // run an SQL statement and capture the result set
-            String sql = "SELECT * FROM users WHERE lastName = ?";
+            String sql = "UPDATE users SET firstName = ? WHERE id = ?";
             System.out.println(sql);
+
             stmt = createPreparedStatement(sql);
 
-            stmt.setString(1, lastNameValue);
+            stmt.setString(1, firstNameValue);
+            stmt.setInt(2, idValue);
 
-            rs = stmt.executeQuery();
+            int updatedRows = stmt.executeUpdate();
+            System.out.println("Rows updated = " + updatedRows);
+
+            //rs = stmt.executeQuery();
 
             // Parse result set
-            while (rs.next()) {
-                User user = new User();
-
-                user.setId(rs.getInt("id"));
-                user.setFirstName(rs.getString("firstName"));
-                user.setLastName(rs.getString("lastName"));
-                user.setEmail(rs.getString("email"));
-                user.setPassword(rs.getString("password"));
-                user.setPhone(rs.getString("phone"));
-                user.setAddress(rs.getString("address"));
-                user.setCity(rs.getString("city"));
-                user.setState(rs.getString("state"));
-                user.setZipCode(rs.getString("zipCode"));
-
-                users.add(user);
-            }
+            //while (rs.next()) {
+            //    User user = new User();
+            //
+            //    user.setId(rs.getInt("id"));
+            //    user.setFirstName(rs.getString("firstName"));
+            //    user.setLastName(rs.getString("lastName"));
+            //    user.setEmail(rs.getString("email"));
+            //    user.setPassword(rs.getString("password"));
+            //    user.setPhone(rs.getString("phone"));
+            //    user.setAddress(rs.getString("address"));
+            //    user.setCity(rs.getString("city"));
+            //    user.setState(rs.getString("state"));
+            //    user.setZipCode(rs.getString("zipCode"));
+            //
+            //    users.add(user);
+            //}
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -123,6 +128,95 @@ public class UserDao extends AbstractDao {
             }
         }
 
-        return users;
+        //return users;
     }
+
+    public void insertUser() {
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+
+        List<User> users = new ArrayList<>();
+
+        try {
+            // run an SQL statement and capture the result set
+            String sql = "INSERT INTO `users` (`firstName`, `lastName`, `email`, `password`, `phone`, `address`, `city`, `state`, `zipCode`) VALUES ('Wasin2', 'Sanguansuk2', 'wasin2.sanguansuk2@gmail.com', '123456', '(773) 999-8888', '1280 N Milwaukee Ave Apt 1101', 'Chicago', 'IL', '60642');";
+            System.out.println(sql);
+
+            stmt = createPreparedStatement(sql);
+
+            //stmt.setString(1, firstNameValue);
+            //stmt.setInt(2, idValue);
+
+            int updatedRows = stmt.executeUpdate();
+            System.out.println("Rows updated = " + updatedRows);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (stmt != null) {
+                        stmt.close();
+                    }
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                } finally {
+                    //closeConnection();
+                }
+            }
+        }
+
+        //return users;
+    }
+
+    public void deleteUser(Integer idValue) {
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+
+        List<User> users = new ArrayList<>();
+
+        try {
+            // run an SQL statement and capture the result set
+            String sql = "DELETE FROM users WHERE id = ?;";
+            System.out.println(sql);
+
+            stmt = createPreparedStatement(sql);
+
+            stmt.setInt(1, idValue);
+            System.out.println(sql);
+
+            int updatedRows = stmt.executeUpdate();
+            System.out.println("Rows updated = " + updatedRows);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (stmt != null) {
+                        stmt.close();
+                    }
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                } finally {
+                    //closeConnection();
+                }
+            }
+        }
+
+        //return users;
+    }
+
 }
